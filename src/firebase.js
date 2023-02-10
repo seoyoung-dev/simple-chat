a1e54ec5fd769a93339da20f547c5384a03b1b2c
seoyoung-dev <seoyoung.dev@gmail.com>
2023-02-10 21:32:47 +0900

    chore: install firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyB1vo5edRbWPFV4XeYw2Hc648DRytmaaxQ',
+    authDomain: 'react-firebase-chat-app-a324a.firebaseapp.com',
+    projectId: 'react-firebase-chat-app-a324a',
+    storageBucket: 'react-firebase-chat-app-a324a.appspot.com',
+    messagingSenderId: '823322500591',
+    appId: '1:823322500591:web:3d89415efe900775202a1a'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
