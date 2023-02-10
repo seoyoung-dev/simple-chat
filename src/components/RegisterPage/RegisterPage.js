6018dff150a514524e17973b5adfe312818ef570
seoyoung-dev <seoyoung.dev@gmail.com>
2023-02-10 23:49:07 +0900

    feat: 회원가입 기능 구현

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,7 +1,53 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 function RegisterPage() {
-    return <div>RegisterPage</div>;
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors }
+    } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    }; // your form submit function which will invoke after successful validation
+
+    console.log(watch('example'));
+    return (
+        <div className="auth-wrapper">
+            <div style={{ textAlign: 'center', marginBottom: '30px' }}>
+                <h3>Register</h3>
+            </div>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Email</label>
+                <input
+                    type="email"
+                    {...register('email', { required: true })}
+                />
+                <label>Name</label>
+                <input {...register('name', { required: true })} />
+                <label>Password</label>
+                <input
+                    type="password"
+                    {...register('password', { required: true })}
+                />
+                <label>Password Confirm</label>
+                <input
+                    type="password"
+                    {...register('password_confirm', { required: true })}
+                />
+
+                {errors.exampleRequired && <span>This field is required</span>}
+
+                <input type="submit" />
+            </form>
+            <Link to={'/login'} id="link">
+                이미 아이디가 있나요?
+            </Link>
+        </div>
+    );
 }
 
 export default RegisterPage;
