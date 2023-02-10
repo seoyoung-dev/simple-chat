8f69e9dedadacc7cc7478bc07a5ff8854c780267
seoyoung-dev <seoyoung.dev@gmail.com>
2023-02-10 23:07:01 +0900

    fix: change createStore function code

--- src/index.js
+++ src/index.js
@@ -3,28 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import promiseMiddleware from 'redux-promise';
-import ReduxThunk from 'redux-thunk';
-import Reducer from './redux/reducers';
-
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
+import rootReducer from './redux/reducers';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const createStoreWithMiddleware = applyMiddleware(
-    promiseMiddleware,
-    ReduxThunk
-)(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk, promise))
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Provider
-            store={createStoreWithMiddleware(
-                Reducer,
-                window.__REDUX_DEVTOOLS_EXTENSION__ &&
-                    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-            )}
-        >
+        <Provider store={store}>
             <App />
         </Provider>
     </React.StrictMode>
