8f362b348e2e5461ea8ded91cb246d04326a2eae
seoyoung-dev <seoyoung.dev@gmail.com>
2023-02-13 03:05:55 +0900

    feat: 회원정보 redux store에 저장

--- src/App.js
+++ src/App.js
@@ -7,28 +7,41 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import { auth } from './firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from './redux/actions/user_action';
+
 function App() {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const isLoading = useSelector((state) => {
+        console.log(state);
+    });
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
-            console.log('user', user);
             if (user) {
                 // 로그인이 된 상태
                 navigate('/');
+                dispatch(setUser(user));
+                console.log('user', user);
             } else {
                 // 로그인 되지 않은 상태
                 navigate('/login');
             }
         });
     }, []);
-    return (
-        <Routes>
-            <Route path="/" element={<ChatPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-    );
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <Routes>
+                <Route path="/" element={<ChatPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+            </Routes>
+        );
+    }
 }
 
 export default App;
