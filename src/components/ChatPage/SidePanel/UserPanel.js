a91da8aa32b64b2937778b339e5fb73a02963f1b
seoyoung-dev <seoyoung.dev@gmail.com>
2023-02-13 04:13:45 +0900

    feat: 채팅 페이지 내에서 로그인한 유저 정보 보여주기

--- src/components/ChatPage/SidePanel/UserPanel.js
+++ src/components/ChatPage/SidePanel/UserPanel.js
@@ -1,5 +1,45 @@
+import React from 'react';
+import { RiChatSmile2Line } from 'react-icons/ri';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Image from 'react-bootstrap/Image';
+import { useSelector } from 'react-redux';
+
 function UserPanel() {
-    return <div>UserPannel</div>;
+    const user = useSelector((state) => state.user.currentUser);
+
+    return (
+        <div>
+            <h3 style={{ color: 'white' }}>
+                {' '}
+                <RiChatSmile2Line />
+                Simple Chat
+            </h3>
+            <div style={{ display: 'flex', marginBottom: '1rem' }}>
+                <Image
+                    src={user && user.photoURL}
+                    roundedCircle
+                    style={{ width: '30p', height: '30px', marginTop: '3px' }}
+                ></Image>
+                <Dropdown>
+                    <Dropdown.Toggle
+                        id="dropdown-basic"
+                        style={{ background: 'transparent', border: '0px' }}
+                    >
+                        {user && user.displayName}
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">
+                            프로필 사진 변경
+                        </Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">
+                            로그아웃
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
+        </div>
+    );
 }
 
 export default UserPanel;
