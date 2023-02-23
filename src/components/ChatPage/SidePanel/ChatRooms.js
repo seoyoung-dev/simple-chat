ab061c4997b6be20553d72bb8c4f55988a6a5873
seoyoung-dev <seoyoung.dev@gmail.com>
2023-02-24 00:42:56 +0900

    feat: 새로운 채팅룸 생성 모달

--- src/components/ChatPage/SidePanel/ChatRooms.js
+++ src/components/ChatPage/SidePanel/ChatRooms.js
@@ -1,5 +1,80 @@
-function ChatRooms() {
-    return <div>Chatrooms</div>;
-}
+import React, { Component, useState } from 'react';
+import { FaRegSmileWink } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
-export default ChatRooms;
+export default class ChatRooms extends Component {
+    state = {
+        show: false
+    };
+
+    handleClose = () => this.setState({ show: false });
+    handleShow = () => this.setState({ show: true });
+
+    render() {
+        return (
+            <div>
+                <div
+                    style={{
+                        position: 'relative',
+                        width: '100%',
+                        display: 'flex',
+                        alignItems: 'center'
+                    }}
+                >
+                    <FaRegSmileWink style={{ marginRight: '3px' }} />
+                    CHAT ROOMS
+                    <FaPlus
+                        onClick={this.handleShow}
+                        style={{
+                            cursor: 'pointer',
+                            position: 'absolute',
+                            right: 0
+                        }}
+                    />
+                </div>
+
+                <Modal show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>채팅방 생성</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="formBasicEmail"
+                            >
+                                <Form.Label>방 이름</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Enter a chat room name"
+                                />
+                            </Form.Group>
+
+                            <Form.Group
+                                className="mb-3"
+                                controlId="formBasicPassword"
+                            >
+                                <Form.Label>방 설명</Form.Label>
+                                <Form.Control
+                                    type="description"
+                                    placeholder="Enter a chat room description"
+                                />
+                            </Form.Group>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleClose}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        );
+    }
+}
