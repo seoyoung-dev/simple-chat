e20fc6269c53a87538ec6dd4aafd021fc7c81eee
seoyoung-dev <seoyoung.dev@gmail.com>
2023-02-28 23:45:00 +0900

    feat: 새로운 채팅방 생성

--- src/components/ChatPage/SidePanel/ChatRooms.js
+++ src/components/ChatPage/SidePanel/ChatRooms.js
@@ -4,14 +4,57 @@ import { FaPlus } from 'react-icons/fa';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import { connect } from 'react-redux';
+import { database } from '../../../firebase';
+import { ref, push, set, update, child } from 'firebase/database';
 
-export default class ChatRooms extends Component {
+class ChatRooms extends Component {
     state = {
-        show: false
+        show: false,
+        name: '',
+        description: '',
+        chatRoomsRef: ref(database, 'chatRooms')
     };
 
     handleClose = () => this.setState({ show: false });
     handleShow = () => this.setState({ show: true });
+    isFormValid = (name, description) => name && description;
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { name, description } = this.state;
+
+        if (this.isFormValid(name, description)) {
+            this.addChatRoom();
+        } else {
+            alert('내용을 입력하세요.');
+        }
+    };
+    addChatRoom = async () => {
+        const key = push(this.state.chatRoomsRef).key;
+        const { name, description } = this.state;
+        const { user } = this.props;
+        const newChatRoom = {
+            id: key,
+            name: name,
+            description: description,
+            createdBy: {
+                name: user.displayName,
+                image: user.photoURL
+            }
+        };
+
+        try {
+            await update(child(this.state.chatRoomsRef, key), newChatRoom);
+            this.setState({
+                name: '',
+                description: '',
+                show: false
+            });
+        } catch (error) {
+            alert(error);
+        }
+    };
 
     render() {
         return (
@@ -41,7 +84,7 @@ export default class ChatRooms extends Component {
                         <Modal.Title>채팅방 생성</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <Form>
+                        <Form onSubmit={this.handleSubmit}>
                             <Form.Group
                                 className="mb-3"
                                 controlId="formBasicEmail"
@@ -50,6 +93,9 @@ export default class ChatRooms extends Component {
                                 <Form.Control
                                     type="email"
                                     placeholder="Enter a chat room name"
+                                    onChange={(e) =>
+                                        this.setState({ name: e.target.value })
+                                    }
                                 />
                             </Form.Group>
 
@@ -61,16 +107,21 @@ export default class ChatRooms extends Component {
                                 <Form.Control
                                     type="description"
                                     placeholder="Enter a chat room description"
+                                    onChange={(e) =>
+                                        this.setState({
+                                            description: e.target.value
+                                        })
+                                    }
                                 />
                             </Form.Group>
                         </Form>
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={this.handleClose}>
-                            Close
+                            닫기
                         </Button>
-                        <Button variant="primary" onClick={this.handleClose}>
-                            Save Changes
+                        <Button variant="primary" onClick={this.handleSubmit}>
+                            방 생성
                         </Button>
                     </Modal.Footer>
                 </Modal>
@@ -78,3 +129,11 @@ export default class ChatRooms extends Component {
         );
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        user: state.user.currentUser
+    };
+};
+
+export default connect(mapStateToProps)(ChatRooms);
