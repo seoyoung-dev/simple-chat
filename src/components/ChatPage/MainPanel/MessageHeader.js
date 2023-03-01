d8d4b5ca7171b6d544b7f5762c5c1c94784cc16e
seoyoung-dev <seoyoung.dev@gmail.com>
2023-03-02 00:14:01 +0900

    feat: 메세지 헤더 UI 생성

--- src/components/ChatPage/MainPanel/MessageHeader.js
+++ src/components/ChatPage/MainPanel/MessageHeader.js
@@ -1,5 +1,103 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { FaLock } from 'react-icons/fa';
+import { MdFavorite } from 'react-icons/md';
+import { AiOutlineSearch } from 'react-icons/ai';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Image from 'react-bootstrap/Image';
+import Accordion from 'react-bootstrap/Accordion';
+import { useAccordionButton } from 'react-bootstrap/AccordionButton';
+import Card from 'react-bootstrap/Card';
+
 function MessageHeader() {
-    return <div>MessageHeader</div>;
+    const CustomToggle = ({ children, eventKey }) => {
+        const decoratedOnClick = useAccordionButton(eventKey, () =>
+            console.log('totally custom!')
+        );
+
+        return (
+            <button
+                type="button"
+                style={{ backgroundColor: 'pink' }}
+                onClick={decoratedOnClick}
+            >
+                {children}
+            </button>
+        );
+    };
+    return (
+        <div
+            style={{
+                width: '100%',
+                height: '170px',
+                border: '.2rem solid #ececec',
+                borderRadius: '4px',
+                padding: '1rem',
+                marginBottom: '1rem'
+            }}
+        >
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>
+                            <FaLock />
+                            Room name
+                            <MdFavorite />
+                        </h2>
+                    </Col>
+                    <Col>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text id="basic-addon1">
+                                <AiOutlineSearch />
+                            </InputGroup.Text>
+                            <Form.Control
+                                placeholder="Search Messages"
+                                aria-label="Search"
+                                aria-describedby="basic-addon1"
+                            />
+                        </InputGroup>
+                    </Col>
+                </Row>
+                <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <p>
+                        <Image />
+                    </p>
+                </div>
+                <Row>
+                    <Col>
+                        <Accordion>
+                            <Card>
+                                <Card.Header style={{ padding: '0 1rem' }}>
+                                    <CustomToggle eventKey="0">
+                                        Descprio
+                                    </CustomToggle>
+                                </Card.Header>
+                                <Accordion.Collapse eventKey="0">
+                                    <Card.Body>Hello! I'm the body</Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        </Accordion>
+                    </Col>
+                    <Col>
+                        <Accordion>
+                            <Card>
+                                <Card.Header style={{ padding: '0 1rem' }}>
+                                    <CustomToggle eventKey="0">
+                                        Click me!
+                                    </CustomToggle>
+                                </Card.Header>
+                                <Accordion.Collapse eventKey="0">
+                                    <Card.Body>Hello! I'm the body</Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        </Accordion>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default MessageHeader;
